56718c15e9e75acbaf90cb4be357aa7408cdf4cd
emmitan <142547879+emmitan@users.noreply.github.com>
2024-02-22 09:16:19 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+pygame.display.set_caption('Bunny Break')
+color = (255, 255, 255)
+screen.fill((color))
+
+pygame.display.flip()
+
+running = True
+while running:
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+pygame.quit()
 
