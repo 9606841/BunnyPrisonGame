30293f16e7b08b795435a5bc4d5e307fa9896db4
emmitan <142547879+emmitan@users.noreply.github.com>
2024-02-22 10:30:20 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,37 @@
 import pygame
+from pygame.constants import KEYDOWN, KEYUP
+#from Bunny import Bunny
 
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([500, 250])
 pygame.display.set_caption('Bunny Break')
 color = (255, 255, 255)
 screen.fill((color))
+gameStart = False
+#bunny = Bunny
 
 pygame.display.flip()
 
 running = True
 while running:
 
+  if pygame.key.get_pressed()[pygame.K_SPACE] and pygame.KEYDOWN:
+    gameStart = True
+    print('Game Starting')
+    c2 = (0, 0, 0)
+    pygame.draw.rect(screen, c2, pygame.Rect(60, 60, 60, 60))
+    pygame.display.flip()
+
+  elif pygame.key.get_pressed()[pygame.K_SPACE] and pygame.KEYUP:
+    color = (255, 255, 255)
+    screen.fill(color)
+
+  else:
+    pass
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
 pygame.quit()
-
