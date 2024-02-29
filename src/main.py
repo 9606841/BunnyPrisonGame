d266d34ca96bfac9af183c6f7419a35265714cbd
emmitan <142547879+emmitan@users.noreply.github.com>
2024-02-29 09:41:08 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ pygame.init()
 
 screen = pygame.display.set_mode([500, 250])
 pygame.display.set_caption('Bunny Break')
-color = (255, 255, 255)
-screen.fill((color))
+bg = pygame.image.load('PrisonBg.png')
+bg = pygame.transform.scale(bg, (500, 250))
+screen.blit(bg, (0, 0))
 gameStart = False
 #b = Bunny(250, 125, 0, False)
-
 pygame.display.flip()
 
 running = True
@@ -19,19 +19,17 @@ while running:
     gameStart = True
 
   if gameStart == True:
-    #c2 = (0, 0, 0)
-    #pygame.display.flip()
-    bg = pygame.image.load('PrisonBg.png')
-    bg = pygame.transform.scale(bg, (500, 250))
-    screen.blit(bg, (0, 0))
     #b.display
+    color = (255, 255, 255)
+    screen.fill((color))
     pygame.display.flip()
-    
+
     #instantiate bunny class & first minigame
 
   else:
-    color = (255, 255, 255)
-    screen.fill(color)
+    bg = pygame.image.load('PrisonBg.png')
+    bg = pygame.transform.scale(bg, (500, 250))
+    screen.blit(bg, (0, 0))
     pygame.display.flip()
 
   for event in pygame.event.get():
