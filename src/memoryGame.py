79a3a5705884a18b3dd0d64643f654349b88d1d7
9606841 <142921807+9606841@users.noreply.github.com>
2024-03-05 09:31:56 -0700

    Update memoryGame.py

--- src/memoryGame.py
+++ src/memoryGame.py
@@ -4,7 +4,7 @@ class MemoryGame:
     self.running = True
     def MemoryGame():
       pygame.init()
-      if pygame.key.get_pressed()[pygame.K_SPACE] and #running:
+      if pygame.key.get_pressed()[pygame.K_SPACE]:
        pygame.display.set_caption('Memory Game, click to continue')
       elif pygame.mouse.get_pressed()[0]:
         running = False
