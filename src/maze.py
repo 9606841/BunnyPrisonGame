c4f8913578c9386ab2fc8c4e83223c1c2b9c10dd
9606841 <142921807+9606841@users.noreply.github.com>
2024-03-05 09:26:03 -0700

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -1,1 +1,28 @@
+#from Cell import Cell
+
+import pygame
+
+pygame.init()
+
+#maze class
+# render squares (path), collision detection for walls, create endpoints
+
+
+class Maze:
+
+  def __init__(self, col, row, running):
+    self.col = col
+    self.row = row
+    self.thickness = 4
+    self.gridCells = 5
+    self.running = running
+    
+  def Maze(self):
+      pygame.init()
+      if pygame.key.get_pressed()[pygame.K_SPACE] and self.running:
+        pygame.display.set_caption('Memory Game, click to continue')
+      elif pygame.mouse.get_pressed()[0]:
+        self.running = False
+      #Cell(cols, rows, self.thickness) for rows in range(self.row)
+      #for cols in range(self.col)
 
