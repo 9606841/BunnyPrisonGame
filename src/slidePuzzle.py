aaad3c59e01dae23a32d9d2644c196f7421f342a
emmitan <142547879+emmitan@users.noreply.github.com>
2024-03-05 09:27:42 -0700

    Update slidePuzzle.py

--- src/slidePuzzle.py
+++ src/slidePuzzle.py
@@ -1,1 +1,10 @@
-
+import pygame
+class slidePuzzle:
+  def __init__(self, running):
+    self.running = True
+    def slidePuzzle():
+      pygame.init()
+      if pygame.key.get_pressed()[pygame.K_SPACE] and running:
+       pygame.display.set_caption('Memory Game, click to continue')
+      elif pygame.mouse.get_pressed()[0]:
+        self.running = False
