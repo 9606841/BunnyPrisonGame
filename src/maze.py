562a375f1a842c7b54960fba6b5a5422c3d0bf05
9606841 <142921807+9606841@users.noreply.github.com>
2024-03-07 09:28:08 -0700

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -20,7 +20,7 @@ class Maze:
   def Maze(self):
       pygame.init()
       if pygame.key.get_pressed()[pygame.K_SPACE] and self.running:
-        pygame.display.set_caption('Memory Game, click to continue')
+        pygame.display.set_caption('Maze, click to continue')
       elif pygame.mouse.get_pressed()[0]:
         self.running = False
       #Cell(cols, rows, self.thickness) for rows in range(self.row)
