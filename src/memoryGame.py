e1f0631d23bdab0258af2725f3f5a9796aba7bfe
9606841 <142921807+9606841@users.noreply.github.com>
2024-03-07 10:17:52 -0700

    Update memoryGame.py

--- src/memoryGame.py
+++ src/memoryGame.py
@@ -1,60 +1,31 @@
 import pygame
+import numpy
+
 class MemoryGame:
-  def __init__(self, running):
-    self.running = True
-    def MemoryGame():
-      pygame.init()
-      if pygame.key.get_pressed()[pygame.K_SPACE]:
-       pygame.display.set_caption('Memory Game, click to continue')
-      elif pygame.mouse.get_pressed()[0]:
-        running = False
-#def MermoryGame():
-  #pygame.init()
-  #screen = pygame.display.set_mode(500,500)
-  #pygame.display.set_caption('Memory Game')
-  #colors = (Color(199, 41, 10), Color(36, 102, 10), Color(81, 28, 97))
-#def mousedPressed:
+  xcard_stack = numpy.array((0,37,74, 111, 148, 0, 37,74, 111, 148, 0, 37,74, 111, 148))
+  ycard_stack = numpy.array((0, 0, 0, 0, 0, 25, 25, 25, 25, 25, 75, 75, 75, 75, 75))
+  def __init__(self, back, word, width, height, x, y):
+    self.back = random(0,2)
+    self.word = word
+    self.width = 25
+    self.height = 37
+    self.x = x
+    self.y = y
 
-# display function?: meDisplay()
-
-import pygame
-import os
-
-#initial module
-pygame.init()
-
-#set up display
-width = 600
-height = 600
-screen = pygame.display.set_mode((width, height))
-
-#upload images
-image_folder = "images"  # folder containing images
-image_files = sorted([os.path.join(image_folder, f) for f in os.listdir(image_folder) if
-                      f.endswith("heartfront.png")])
-
-images = [pygame.image.load(img) for img in image_files]
-
-#set clock
-fps = 3
-clock = pygame.time.Clock()
-
-#main loop
-running = True
-frame = 0
-while running:
-  screen.fill(0, 0, 0)
-  screen.blit(images[frame], (0,0)) #draw first frame
-  frame = (frame + 1) % len(images) #update frame
-  pygame.display.flip() #update display
-
-  #check for events
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-
-  #limit frame rate
-  clock.tick(fps)
-
-pygame.quit()
-
+#loading image backs cards setup
+def backDisplay(self):
+  if self.back = 0:
+    surface = pygame.display.set_mode(self.width,self.height)
+    backImg = ('Card1.png')
+    surface.blit(backImg, (self.width, self.height))
+    pygame.display.flip
+  elif self.back = 1:
+    surface = pygame.display.set_mode(self.width,self.height)
+    backImg = ('Card2.png')
+    surface.blit(backImg, (self.width, self.height))
+    pygame.display.flip
+  elif self.back = 2:
+    surface = pygame.display.set_mode(self.width,self.height)
+    backImg = ('Card3.png')
+    surface.blit(backImg, (self.width, self.height))
+    pygame.display.flip
