1af9a293dad2ed45cd437666acb86683d91825b9
9606841 <142921807+9606841@users.noreply.github.com>
2024-03-08 20:08:16 -0700

    Update memoryGame.py

--- src/memoryGame.py
+++ src/memoryGame.py
@@ -1,11 +1,14 @@
 import pygame
+import random
+import subprocess
+subprocess.run(['pip', 'install', 'numpy'])
+# import numpy still errors
 import numpy
-
 class MemoryGame:
   xcard_stack = numpy.array((0,37,74, 111, 148, 0, 37,74, 111, 148, 0, 37,74, 111, 148))
   ycard_stack = numpy.array((0, 0, 0, 0, 0, 25, 25, 25, 25, 25, 75, 75, 75, 75, 75))
   def __init__(self, back, word, width, height, x, y):
-    self.back = random(0,2)
+    self.back = random.randint(0,2)
     self.word = word
     self.width = 25
     self.height = 37
@@ -14,18 +17,31 @@ class MemoryGame:
 
 #loading image backs cards setup
 def backDisplay(self):
-  if self.back = 0:
+  if self.back == 0:
     surface = pygame.display.set_mode(self.width,self.height)
-    backImg = ('Card1.png')
+    backImg = pygame.image.load('Card1.png')
     surface.blit(backImg, (self.width, self.height))
     pygame.display.flip
-  elif self.back = 1:
+  elif self.back == 1:
     surface = pygame.display.set_mode(self.width,self.height)
-    backImg = ('Card2.png')
+    backImg = pygame.image.load('Card2.png')
     surface.blit(backImg, (self.width, self.height))
     pygame.display.flip
-  elif self.back = 2:
+  elif self.back == 2:
     surface = pygame.display.set_mode(self.width,self.height)
-    backImg = ('Card3.png')
+    backImg = pygame.image.load('Card3.png')
     surface.blit(backImg, (self.width, self.height))
     pygame.display.flip
+
+def applyWords(self, word):
+  display_surface = pygame.display.set_mode((self.x, self.y))
+  pygame.display.set_caption('BunnyBreak')
+  #insert font
+  color = (23, 68, 69)
+  font = pygame.font.SysFont('Arial', 30)
+  text = font.render(self.word, True, color)
+  textRect = text.get_rect()
+  textRect.center= (self.x//2, self.y//2)
+  while True:
+    display_surface.fill((255,255,255))
+    display_surface.blit(text, textRect)
