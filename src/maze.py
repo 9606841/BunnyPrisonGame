08eda635afc904ed669bf435d1be2b22c87ea3d1
emmitan <142547879+emmitan@users.noreply.github.com>
2024-03-13 23:41:49 -0600

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -1,12 +1,16 @@
 #Elisa Tandra
 
 import pygame
-from pygame.locals import *
+from pygame import *
+
+screen = pygame.display.set_mode([500, 250])
+
 # get code to show up on screen
 # create end game logic
 # adjust graphics/colors(designers)
 
-class Maze(): 
+
+class Maze():
   TILE_SIZE = 64
   WIDTH = TILE_SIZE * 4
   HEIGHT = TILE_SIZE * 4
@@ -18,46 +22,40 @@ class Maze():
 
   tiles = [empty, wall, goal]
 
-  maze = [
-      [0,0,0,0,0,0,1,1],
-      [0,0,1,0,0,1,1,0],
-      [0,0,0,0,0,0,0,0],
-      [0,0,1,1,0,1,0,0],
-      [0,0,1,0,1,0,0,1],
-      [1,0,1,0,1,1,0,0],
-      [0,1,0,0,0,1,0,1],
-      [0,0,0,0,0,0,0,2]
-  ]
+  maze = [[0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 1, 0, 0, 1, 1, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1, 0, 0],
+          [0, 0, 1, 0, 1, 0, 0, 1], [1, 0, 1, 0, 1, 1, 0, 0],
+          [0, 1, 0, 0, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0, 2]]
 
   surface = pygame.display.set_mode((500, 500))
   color = (22, 27, 41)
 
-  def draw():
-      for row in range(len(maze)):
-          for column in range(len(maze[row])):
-              x = column * TILE_SIZE
-              y = row * TILE_SIZE
-              tile = pygame.transform.scale(tiles[maze[row][column]], (64,64))
-              screen.blit(tile, (x, y))
-      pygame.display.flip()
-
+  def draw(self):
+    for row in range(len(maze)):
+      for column in range(len(maze[row])):
+        x = column * TILE_SIZE
+        y = row * TILE_SIZE
+        tile = pygame.transform.scale(tiles[maze[row][column]], (64, 64))
+        screen.blit(tile, (x, y))
+    pygame.display.flip()
 
   px = 20
   py = 20
   run = True
+
   def mazeRun():
-    screen.fill((255,255,255))
+    screen.fill((255, 255, 255))
     draw()
-    player = pygame.draw.ellipse(screen, color, (px, py, 20,20))
+    player = pygame.draw.ellipse(screen, color, (px, py, 20, 20))
     pygame.display.flip()
     for event in pygame.event.get():
-     if event.type == KEYDOWN:
+      if event.type == KEYDOWN:
         r = py
         c = px
         if event.key == K_UP:
           r = r - TILE_SIZE
         if event.key == K_DOWN:
-         r = r + TILE_SIZE
+          r = r + TILE_SIZE
         if event.key == K_LEFT:
           c = c - TILE_SIZE
         if event.key == K_RIGHT:
