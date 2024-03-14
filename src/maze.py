7351fde49a9970f42dc4a93e8defd05116dbf513
emmitan <142547879+emmitan@users.noreply.github.com>
2024-03-13 20:48:29 -0600

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -6,7 +6,6 @@ pygame.init()
 # render squares (path), collision detection for walls, create endpoints
 # use wall img as collision detection and then force bunny to walk on path (seprate image) only - img might not work, will hv to render squares
 
-
 class Maze:
 
   def __init__(self, col, row, running):
@@ -15,16 +14,17 @@ class Maze:
     self.running = running
     
   def Maze(self):
-      pygame.init()
-      if pygame.key.get_pressed()[pygame.K_SPACE] and self.running:
+    pygame.init()
+    if pygame.key.get_pressed()[pygame.K_SPACE] and self.running:
         pygame.display.set_caption('Maze, click to continue')
-      elif pygame.mouse.get_pressed()[0]:
+    elif pygame.mouse.get_pressed()[0]:
         self.running = False
     # maze wall
     #wall = pygame.image.load('mazewall.png')
     #wall = pygame.transform.scale(wall, (250, 250))  
     bl = pygame.Color(0, 0, 0)
-  ` col = pygame.draw.rect(screen, bl, (0, 0, 20, 40))
+    scr = 
+  ` col = pygame.draw.rect(scr, bl, (0, 0, 20, 40))
   
   # display the maze
   def maDisplay():
